8b57d244ed92a5967bf3b5d777fbac9aacee24bb
javiermfriedman <147084311+javiermfriedman@users.noreply.github.com>
2025-03-05 15:31:07 +0100

    Initial Commit

---
+++ Tinto Time/Tinto_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinto_TimeApp.swift
+//  Tinto Time
+//
+//  Created by Javier Friedman on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Tinto_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tinto TimeTests/Tinto_TimeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tinto_TimeTests.swift
+//  Tinto TimeTests
+//
+//  Created by Javier Friedman on 3/5/25.
+//
+
+import Testing
+@testable import Tinto_Time
+
+struct Tinto_TimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
